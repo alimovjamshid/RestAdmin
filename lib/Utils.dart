58cc9c198c7016256e1b8c785e1a7f80089e41b7
jamshid <jamshidalimov3464@gmail.com>
2023-09-19 19:00:30 +0500

    Wedding Api full

--- lib/Utils.dart
+++ lib/Utils.dart
@@ -2,8 +2,6 @@ import 'dart:convert';
 
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'package:shared_preferences/shared_preferences.dart';
 
 class Utils {
   final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
